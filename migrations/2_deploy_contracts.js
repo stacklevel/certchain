81512025b9b57568d23653c90786694b40ddc657
Eugene Traytyak <evgen.traytyak@gmail.com>
2017-09-09 21:10:11 +0300

    Add CertCoin contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,11 +2,14 @@ var Auditor = artifacts.require("./Auditor.sol");
 var Manufacturer = artifacts.require("./Manufacturer.sol");
 var Organ = artifacts.require("./Organ.sol");
 var CertOrder = artifacts.require("./CertOrder.sol");
+var CertCoin = artifacts.require("./CertCoin.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Auditor).then(function() {
+  deployer.deploy(CertCoin, 1000, 'CertCoin', 8, 'CRT').then(function() {
+    deployer.deploy(Auditor, CertCoin.address).then(function() {
       deployer.deploy(CertOrder, Auditor.address)
+    });
+    deployer.deploy(Manufacturer);
+    deployer.deploy(Organ);
   });
-  deployer.deploy(Manufacturer);
-  deployer.deploy(Organ);
 };
