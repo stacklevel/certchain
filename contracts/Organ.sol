f189375beff943f49aa1a89796296e96cbfa9f0d
Mikhail Dyomin <mikhaildyomin@gmail.com>
2017-09-12 22:17:59 +0300

    Fix linting errors

--- contracts/Organ.sol
+++ contracts/Organ.sol
@@ -2,8 +2,7 @@ pragma solidity ^0.4.4;
 
 
 contract Organ {
-
-    struct organ {
+    struct OrganInfo {
         bytes32 name;
         bytes32 addr;
         bytes32 phoneNumber;
@@ -11,21 +10,21 @@ contract Organ {
         address nextAddr;
     }
 
-    mapping (address => organ) public organInfo;
+    mapping (address => OrganInfo) public organInfo;
     address public headAddr;
 
-    event LogOrganRegistered(address accountAddress, bytes32 name);
-
-    function register(bytes32 name, bytes32 addr, bytes32 phoneNumber, bytes32 email) {
+    function register(
+        bytes32 name,
+        bytes32 addr,
+        bytes32 phoneNumber,
+        bytes32 email) {
         organInfo[msg.sender].name = name;
         organInfo[msg.sender].addr = addr;
         organInfo[msg.sender].phoneNumber = phoneNumber;
         organInfo[msg.sender].email = email;
         organInfo[msg.sender].nextAddr = headAddr;
         headAddr = msg.sender;
-        LogOrganRegistered(msg.sender, organInfo[msg.sender].name);
     }
-
 
     function getByAddress(address organAddress) constant returns (bytes32, bytes32, bytes32, bytes32, address) {
         return (
@@ -36,9 +35,4 @@ contract Organ {
             organInfo[organAddress].nextAddr
         );
     }
-
-    function getHeadAddr() constant returns (address) {
-        return headAddr;
-    }
-
 }
